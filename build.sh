a7913153bc0425554bea5b7ce30f4aaa09c6f970
白渐 <jianbai@creditease.cn>
2016-05-05 15:23:02 +0800

    Fix wrong Dockerfile path when building

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 set -e
+parent=`dirname $0`
+
 tmp_container="heka_instance"
 release_img="heka_release"
 
-docker build -t $release_img .
+docker build -t $release_img $parent
 docker create --name $tmp_container $release_img
 docker cp $tmp_container:/heka_build/heka-lain-0.10.tgz ./heka-lain.tgz
 docker rm -f $tmp_container
